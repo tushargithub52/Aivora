ec37bb8bb920567c528966d5d0dc6eccca3ed2c1
tushargithub52 <tusharraiku6020@gmail.com>
2025-08-22 00:31:21 +0530

    Add cookie-based authentication middleware for socket connections

--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -1,7 +1,34 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model'); 
 
 async function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
+
+  // Middlware to ensure that the socket connection is established only for authenticated users
+  io.use(async (socket, next) => {
+
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || '');
+    const token = cookies.token;
+
+    if (!token) {
+      return next(new Error("Authentication error: No token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = user;
+
+      next();
+    } catch (error) {
+      return next(new Error("Authentication error"));
+    }
+
+  })
 
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
