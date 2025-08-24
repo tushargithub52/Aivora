22729d273b3b161f7d8e96645a8907f4ad5a2cf2
tushargithub52 <tusharraiku6020@gmail.com>
2025-08-24 23:25:27 +0530

    Add CORS middleware for handling cross-origin requests

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 /* Routes import */
 const authRoutes = require('./routes/auth.routes');
@@ -10,6 +11,10 @@ const app = express();
 // use middlewares 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, 
+}));
 
 // use routes
 app.use('/api/auth', authRoutes);
