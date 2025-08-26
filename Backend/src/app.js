38b8b19d672cc83a50304365e4c5b4342697b391
tushargithub52 <tusharraiku6020@gmail.com>
2025-08-27 00:19:15 +0530

    feat: add path module and update static file serving in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 /* Routes import */
 const authRoutes = require('./routes/auth.routes');
@@ -15,10 +16,14 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true, 
 }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // use routes
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
